6e2f510739fa8a22878e549ae493848e5b80efe3
Abdul Wasey <abdulwasey13@gmail.com>
2021-01-30 16:04:43 +0530

    Update settings.py

--- slack_bot/settings.py
+++ slack_bot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jswuhq4=cl*ps%viqdjq+-o)0q-)1of!*sl7u85cke+$%q6)r3'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
